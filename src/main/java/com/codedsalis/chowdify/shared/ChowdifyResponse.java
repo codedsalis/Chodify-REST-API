9009905a5635977527ff658b0d91b69477df9a66
codedsalis <codedsalis@gmail.com>
2023-12-14 00:54:44 +0100

    Add a standard response format

--- src/main/java/com/codedsalis/chowdify/shared/ChowdifyResponse.java
+++ src/main/java/com/codedsalis/chowdify/shared/ChowdifyResponse.java
@@ -1,14 +1,11 @@
-package com.codedsalis.chowdify.food.response;
+package com.codedsalis.chowdify.shared;
 
-import com.codedsalis.chowdify.food.Food;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 @Builder
-public class FoodResponse {
+public class ChowdifyResponse {
     private String status;
 
     private Object data;
