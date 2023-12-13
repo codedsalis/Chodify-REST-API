dd9c6ec575a47db99b5dd2577c365d4eeb13ef12
codedsalis <codedsalis@gmail.com>
2023-12-14 00:55:09 +0100

    Add route to get single food

--- src/main/java/com/codedsalis/chowdify/food/FoodController.java
+++ src/main/java/com/codedsalis/chowdify/food/FoodController.java
@@ -1,7 +1,7 @@
 package com.codedsalis.chowdify.food;
 
 import com.codedsalis.chowdify.food.request.CreateFoodRequest;
-import com.codedsalis.chowdify.food.response.FoodResponse;
+import com.codedsalis.chowdify.shared.ChowdifyResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/food")
@@ -24,23 +25,34 @@ public class FoodController {
     }
 
     @GetMapping
-    public ResponseEntity<FoodResponse> getFoods() {
+    public ResponseEntity<ChowdifyResponse> getAllFoods() {
         List<Food> foods = foodService.getFoods();
 
-        FoodResponse foodResponse = new FoodResponse();
-        foodResponse.setFoods(foods);
+        ChowdifyResponse chowdifyResponse = ChowdifyResponse.builder().status("success").data(foods).build();
 
-        return ResponseEntity.status(HttpStatus.OK).body(foodResponse);
+        return ResponseEntity.status(HttpStatus.OK).body(chowdifyResponse);
     }
 
     @PostMapping
-    public ResponseEntity<FoodResponse> storeFood(@Valid @RequestBody CreateFoodRequest request) {
+    public ResponseEntity<ChowdifyResponse> storeFood(@Valid @RequestBody CreateFoodRequest request) {
         Food createdFood = this.foodService.saveFood(request);
 
-        FoodResponse foodResponse = new FoodResponse();
-        foodResponse.setFood(createdFood);
+        ChowdifyResponse chowdifyResponse = ChowdifyResponse.builder().data(createdFood).build();
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(foodResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(chowdifyResponse);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ChowdifyResponse> getFood(@PathVariable Long id) {
+        Optional<Food> food = foodService.findById(id);
+
+        if (food.isEmpty()) {
+            ChowdifyResponse chowdifyResponse = ChowdifyResponse.builder().status("failed").data("Food not found").build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(chowdifyResponse);
+        }
+
+        ChowdifyResponse chowdifyResponse = ChowdifyResponse.builder().status("success").data(food).build();
+        return ResponseEntity.status(HttpStatus.OK).body(chowdifyResponse);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
